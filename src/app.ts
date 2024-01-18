eac44186401b2d7784527348df6eee21713c24aa
rakib38324 <rakib38324@gmail.com>
2024-01-18 20:36:00 +0600

    update credentials

--- src/app.ts
+++ src/app.ts
@@ -9,7 +9,7 @@ const app: Application = express();
 
 //--->parser
 app.use(express.json());
-app.use(cors({ origin: ['http://localhost:5173'] }));
+app.use(cors({ origin: ['http://localhost:5173'], credentials: true }));
 app.use(cookieParser());
 
 // const test = async(req: Request, res: Response)=>{
